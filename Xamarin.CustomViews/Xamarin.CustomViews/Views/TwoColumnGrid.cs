3fb976db9818d24f4ea1d4120138d405443ac42f
ntgnst <yahsibatuhan@gmail.com>
2018-10-02 15:28:45 +0300

    # Two Column Grid Updated.

--- Xamarin.CustomViews/Xamarin.CustomViews/Views/TwoColumnGrid.cs
+++ Xamarin.CustomViews/Xamarin.CustomViews/Views/TwoColumnGrid.cs
@@ -1,14 +1,18 @@
-﻿using Xamarin.CustomViews.Enums;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
+using Xamarin.CustomViews.Enums;
 using Xamarin.Forms;
 
 namespace Xamarin.CustomViews.Views
 {
-    public class TwoColumnGrid : Grid
+    public class TwoColumnGrid : Grid, IDisposable
     {
         private int _count;
-        private int _lineHeight;
-        private int _starHeight;
-
+        public int _lineHeight;
+        public int _starHeight;
+        private bool _disposed = false;
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
         public TwoColumnGrid(int lineHeight = 0, int starHeight = 0)
         {
             _count = 0;
@@ -28,7 +32,7 @@ namespace Xamarin.CustomViews.Views
             int countRow = _count / 2;
             if (RowDefinitions.Count <= countRow)
             {
-                if (_starHeight > 0 )
+                if (_starHeight > 0)
                 {
                     RowDefinitions.Add(new RowDefinition() { Height = new GridLength(_starHeight, GridUnitType.Star) });
                 }
@@ -36,7 +40,7 @@ namespace Xamarin.CustomViews.Views
                 {
                     RowDefinitions.Add(new RowDefinition() { Height = new GridLength(50, GridUnitType.Auto) });
                 }
-                
+
                 Children.Add(view, 0, countRow);
                 Children.Add(new Line(LineEnum.Vertical) { Margin = _lineHeight > 0 ? new Thickness(1, _lineHeight, 1, _lineHeight) : _lineHeight, HeightRequest = 1 }, 1, countRow);
             }
@@ -45,6 +49,32 @@ namespace Xamarin.CustomViews.Views
                 Children.Add(view, 2, countRow);
             }
             _count++;
+        }
+        public void ClearAndDispose()
+        {
+            if (this.Children.Count > 0)
+            {
+                Children.Clear();
+            }
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+            }
+
+            _disposed = true;
         }
     }
 }
